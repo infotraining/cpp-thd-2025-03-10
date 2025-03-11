98d8fa67a22d1ab22f761a39d41a9d2dce55df62
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-03-11 14:47:43 +0000

    ThreadSafeQueue

--- _exercises/thread-safe-queue/src/thread_safe_queue.hpp
+++ _exercises/thread-safe-queue/src/thread_safe_queue.hpp
@@ -8,6 +8,55 @@
 template <typename T>
 class ThreadSafeQueue
 {
+    std::queue<T> q_;
+    mutable std::mutex mtx_q_;
+    std::condition_variable cv_q_not_empty_;
+public:
+    ThreadSafeQueue() = default;
+
+    bool empty() const
+    {
+        std::lock_guard lk{mtx_q_};
+        return q_.empty();
+    }
+
+    void push(const T& item)
+    {
+        {
+            std::lock_guard lk{mtx_q_};
+            q_.push(item);
+        }
+        cv_q_not_empty_.notify_one();
+    }
+
+    void push(std::initializer_list<T> lst)
+    {
+        {
+            std::lock_guard lk{mtx_q_};
+            for(const auto& item : lst)
+                q_.push(item);
+        }
+        cv_q_not_empty_.notify_all();
+    }
+
+    bool try_pop(T& item)
+    {
+        std::unique_lock lk{mtx_q_, std::try_to_lock};
+        if (!lk.owns_lock() || q_.empty())
+            return false;
+        item = q_.front();
+        q_.pop();
+        return true;
+    }
+
+    void pop(T& item)
+    {
+        std::unique_lock lk{mtx_q_};
+        cv_q_not_empty_.wait(lk, [this] { return !q_.empty(); });
+
+        item = q_.front();
+        q_.pop();
+    }
 };
 
 #endif // THREAD_SAFE_QUEUE_HPP
